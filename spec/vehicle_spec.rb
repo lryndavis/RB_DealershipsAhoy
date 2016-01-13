ec302a493f4d734cc49b4d901363ed34c88762a0
Krystan Menne and Lauryn Davis <lryndavis@gmail.com>
2016-01-13 08:54:08 -0800

    describe vehicle class in specs

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -1,43 +1,45 @@
 require('rspec')
 require('vehicle')
 
-describe('#make') do
-  it('returns the make of the vehicle') do
-    expect(toyota_prius_2000().make()).to(eq("Toyota"))
+describe(Vehicle) do
+  describe('#make') do
+    it('returns the make of the vehicle') do
+      expect(toyota_prius_2000().make()).to(eq("Toyota"))
+    end
   end
-end
 
-describe('#model') do
-  it('returns the model of the vehicle') do
-    expect(toyota_prius_2000().model()).to(eq("Prius"))
+  describe('#model') do
+    it('returns the model of the vehicle') do
+      expect(toyota_prius_2000().model()).to(eq("Prius"))
+    end
   end
-end
 
-describe('#year') do
-  it('returns the year of the vehicle') do
-    expect(toyota_prius_2000().year()).to(eq(2000))
+  describe('#year') do
+    it('returns the year of the vehicle') do
+      expect(toyota_prius_2000().year()).to(eq(2000))
+    end
   end
-end
 
-describe('.all') do
-  it('is empty at first') do
-    expect(Vehicle.all()).to(eq([]))
+  describe('.all') do
+    it('is empty at first') do
+      expect(Vehicle.all()).to(eq([]))
+    end
   end
-end
 
-describe('#save') do
-  it('will save a vehicle to the array') do
-    test_vehicle = toyota_prius_2000()
-    test_vehicle.save()
-    expect(Vehicle.all()).to(eq([test_vehicle]))
+  describe('#save') do
+    it('will save a vehicle to the array') do
+      test_vehicle = toyota_prius_2000()
+      test_vehicle.save()
+      expect(Vehicle.all()).to(eq([test_vehicle]))
+    end
   end
-end
 
-describe('#clear') do
-  it('will clear the array') do
-    toyota_prius_2000().save()
-    Vehicle.clear()
-    expect(Vehicle.all()).to(eq([]))
+  describe('#clear') do
+    it('will clear the array') do
+      toyota_prius_2000().save()
+      Vehicle.clear()
+      expect(Vehicle.all()).to(eq([]))
+    end
   end
 end
 
