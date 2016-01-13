7f66366ec8b52a7246a3c7de52930b2ad19330fc
Krystan Menne and Lauryn Davis <kmenne@gmail.com>
2016-01-13 09:38:02 -0800

    return an empty array of cars for dealership

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,9 +1,22 @@
 class Dealership
+  @@dealerships = []
+
   def initialize(name)
     @name = name
+    @id = @@dealerships.length() + 1
+    @cars = []
   end
 
   def name
     @name
   end
+
+  def id
+    @id
+  end
+
+  def cars
+    @cars
+  end
+
 end
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -1,11 +1,22 @@
 require('rspec')
 require('dealership')
-require('vehicle')
 
 describe(Dealership) do
   describe('#name') do
     it('returns the name of a dealership') do
       expect(bobs_cars.name()).to(eq("Bob's Used Cars"))
+    end
+  end
+
+  describe('#id') do
+    it('will return the id of a dealership') do
+      expect(bobs_cars.id()).to(eq(1))
+    end
+  end
+
+  describe('#cars') do
+    it('returns an empty array of cars for the dealership') do
+      expect(bobs_cars.cars()).to(eq([]))
     end
   end
 end
